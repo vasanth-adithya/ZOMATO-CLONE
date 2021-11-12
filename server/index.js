b3349e5973ece0610dbf8f68f777880860af6140
vasanth-adithya <vamshi4862@gmail.com>
2021-11-12 07:02:19 +0530

    Google Auth and Google Config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+//configs
+import googleAuthConfig from "./config/google.config";
 
 //Routes
 import Auth from "./API/Auth/index";
@@ -11,10 +15,15 @@ import ConnectDB from "./database/connection";
 
 const zomato = express();
 
+//passport config
+googleAuthConfig(passport);
+
 zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(cors());
 zomato.use(helmet());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
 zomato.get("/", (req, res) => {
   res.json({ message: "Setup Success" });
